93147089e32e9d9225315a3d1f5daded7b1a9cd3
Calandda <A.calandda@gmail.com>
2025-07-14 22:33:16 +0800

    reworked early parts of Javascript

--- script/script.js
+++ script/script.js
@@ -1,35 +1,53 @@
 class mainObj{
     constructor(){
-        let currentPress = '';
-        const pLatestKeyPress = '';
-        this.querySelectorAssign();
-        this.eventListenerAll();
-    }
-    buttonPressActivate(){
-        
-    }
-    querySelectorAssign(){
+        const bodyTag = document.querySelector('body');
+        this.currentPress = [];
+        this.buttonHistoryRaw = [];
+        this.buttonHistoryClean = [];
         this.pLatestKeyPress = document.querySelector('.pLatestKeyPress');
-    }
-    eventListenerAll(){
-        const body = document.querySelector('body');
 
-        this.eventListenerKeyboard(body);
-        this.eventListenerMouse(body);
-    }
-    getTime(){
-        return new Date().toLocaleTimeString();
-    }
-    eventListenerKeyboard(elementTag){
-        elementTag.addEventListener(('keydown'),(e)=>{
-            this.currentPress = e.key;
-            this.pLatestKeyPress.textContent = e.key;
+        bodyTag.addEventListener(('keydown'),(e)=>{
+            this.currentPress = this.inputDown(e,this.currentPress, this.getTime());
+        });
+        bodyTag.addEventListener(('keyup'),(e)=>{
+            //this.buttonHistoryRaw.push(this.eventListenerFunctionPress(e));
+        });   
+         elementTag.addEventListener(('mousedown'),(e)=>{
+        });
+        elementTag.addEventListener(('mouseup'), (e)=>{
         });
     }
-    eventListenerMouse(elementTag){
-        elementTag.addEventListener(('mousedown'),(e)=>{
-            this.currentPress = e.button;
-        })
+    getTime(){
+        const options = {hour12: false};
+        const currentTime = new Date().toLocaleTimeString('en-US', options);
+
+        return currentTime;
+    }
+    eventListenerGamepad(elementTag){
+        // Work in Progress
+    }
+    inputDown(buttonStats,currentPress, buttonTime){
+        console.log(buttonStats, currentPress, 'test');
+        let check = false;
+        for (const key of currentPress){
+            if(key['keyCode'] == buttonStats['keyCode']){
+                check = true;
+            }
+        }
+        if(check == true){
+            return currentPress;
+        } else if(check == false){
+            currentPress.push(
+                {
+                    key: buttonStats['key'],
+                    keyCode: buttonStats['keyCode'],
+                    keyTime: buttonTime
+                }
+            );
+            return currentPress;
+        }
+    }
+    inputUp(buttonStats,currentPress, buttonTime){
     }
 }
 
